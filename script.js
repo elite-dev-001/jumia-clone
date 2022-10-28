e81c3881f5578f19479fb279daa402efe18dc403
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-10-28 12:36:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//https://api.apify.com/v2/datasets/Aho45bCRjOsOugEs4/items?clean=true&format=json phone&tablet
+//https://api.apify.com/v2/datasets/GzfQmyagzEy8sAec5/items?clean=true&format=json black friday
+//https://api.apify.com/v2/datasets/IZ4ttpbhx2MIxYezr/items?clean=true&format=json best price
+//https://api.apify.com/v2/datasets/arKAIVoCnCZ2niplY/items?clean=true&format=json free delivery
+//https://api.apify.com/v2/datasets/X8IbJ2jxjW2jPICqU/items?clean=true&format=json traditional wear
+//https://api.apify.com/v2/datasets/sXHUdmKCJLHEdWuM9/items?clean=true&format=json half price
+//https://api.apify.com/v2/datasets/NgopfmIaOx8MGzcy2/items?clean=true&format=json fashion
