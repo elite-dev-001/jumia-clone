1f76d3586f453b5456a255aef730b9c28be25361
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-10-28 16:14:38 +0100

    Jumia

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-//https://api.apify.com/v2/datasets/Aho45bCRjOsOugEs4/items?clean=true&format=json phone&tablet
-//https://api.apify.com/v2/datasets/GzfQmyagzEy8sAec5/items?clean=true&format=json black friday
-//https://api.apify.com/v2/datasets/IZ4ttpbhx2MIxYezr/items?clean=true&format=json best price
-//https://api.apify.com/v2/datasets/arKAIVoCnCZ2niplY/items?clean=true&format=json free delivery
-//https://api.apify.com/v2/datasets/X8IbJ2jxjW2jPICqU/items?clean=true&format=json traditional wear
-//https://api.apify.com/v2/datasets/sXHUdmKCJLHEdWuM9/items?clean=true&format=json half price
-//https://api.apify.com/v2/datasets/NgopfmIaOx8MGzcy2/items?clean=true&format=json fashion
+const itemsArr = ['Supermarket', 'Health & Beauty', 'Home & Office', 'Phones & Tablets', 'Computing', 'Electronics', 'Fashion', 'Baby Products', 'Gaming', 'Sporting goods', 'Automobiles', 'Other categories']
+
+const items = document.querySelector('.items')
+
+itemsArr.forEach((item) => {
+    const p = document.createElement('p')
+    p.innerHTML = item
+    items.appendChild(p)
+})
+
+// setInterval(() => console.log('CAll every 2s'), 2000)
+//CAROUSEL
+
+const imgs = [
+    'images/slider_1.jpeg',
+    'images/slider_2.jpeg',
+    'images/slider_3.jpeg',
+    'images/slider_4.png',
+    'images/slider_5.gif',
+    'images/slider_6.png',
+    'images/slider_7.jpeg',
+]
+
+const carousel = document.querySelector('.carousel')
+const img = document.createElement('img')
+img.src = imgs[0]
+let counter = 1;
+
+
+const circles = Array.from(document.querySelectorAll('.circle'))
+console.log(circles)
+circles[0].setAttribute('style', 'background: #f68b1e')
+
+
+
+// newCircles.forEach((circle) => {
+//     newCircles.indexOf(circle) === counter ? circle.setAttribute('style', 'background: #f68b1e') : circle.setAttribute('style', 'background: #ddd')
+// })
+
+carousel.appendChild(img)
+setInterval(() => {
+    if(counter < imgs.length){
+        img.src = imgs[counter]
+        circles.forEach((circle) => {
+            // console.log(circles.indexOf(circle, counter))
+            circle.setAttribute('style', `background: ${counter === circles.indexOf(circle) ? '#f68b1e' : '#ddd'}`)
+        })
+        counter++
+    }else {
+        counter = 0
+    }
+    
+
+    console.log(counter)
+}, 2000)
+
