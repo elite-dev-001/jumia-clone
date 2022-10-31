8a018d9d3f173cf20181e78b2af98001e10ce794
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-10-31 16:55:03 +0100

    update

--- script.js
+++ script.js
@@ -37,20 +37,98 @@ circles[0].setAttribute('style', 'background: #f68b1e')
 //     newCircles.indexOf(circle) === counter ? circle.setAttribute('style', 'background: #f68b1e') : circle.setAttribute('style', 'background: #ddd')
 // })
 
-carousel.appendChild(img)
+carousel.appendChild(img);
+
 setInterval(() => {
+    // console.log(counter)
     if(counter < imgs.length){
         img.src = imgs[counter]
         circles.forEach((circle) => {
-            // console.log(circles.indexOf(circle, counter))
-            circle.setAttribute('style', `background: ${counter === circles.indexOf(circle) ? '#f68b1e' : '#ddd'}`)
+            circle.setAttribute('style', `background: ${(counter) === circles.indexOf(circle) ? '#f68b1e' : '#ddd'}`)
         })
         counter++
+        // x = undefined;
     }else {
         counter = 0
     }
-    
+        
+}, 7000)
 
-    console.log(counter)
-}, 2000)
+// function mySetInterval(value) {
+//     setTimeout(() => {
+//         if(counter < imgs.length && counter >= 0){
+//             // value === 0 ? counter-- : counter++
+//             img.src = imgs[counter-1]
+//             circles.forEach((circle) => {
+//                 circle.setAttribute('style', `background: ${counter-1 === circles.indexOf(circle) ? '#f68b1e' : '#ddd'}`)
+//             })
+            
+//         }else {
+//             counter < 0 ? counter = (imgs.length - 1) : counter = 0
+//         }
+        
+//     }, 1000)
+// }
+
+/////ARROWS////
+// const arrows = Array.from(document.querySelectorAll('.arrow'))
+
+// function circle(value) {
+//     const div = document.createElement('div')
+//     div.setAttribute('style', 'width: 40px; height: 40px; border-radius: 100%; background: black;color: white; display: flex; justify-content: center; align-items: center')
+//     div.innerHTML = value;
+//     return div;
+// }
+
+// arrows.forEach((arrow) => {
+//     arrow.appendChild(circle(arrows.indexOf(arrow) === 0 ? '<' : '>'))
+    
+//     arrow.addEventListener('click', (e) => {
+//         console.log(counter)
+//         e.preventDefault();
+//         arrows.indexOf(arrow) === 0 ? x = 1: x = 0;
+//         mySetInterval(arrows.indexOf(arrow))
+//     })
+// })
+
+//CONSUMING APIS
+const computings = document.querySelector('.computing')
+
+
+
+axios.get('https://api.apify.com/v2/datasets/1LYsVC3JNAzgf6zBM/items?clean=true&format=json').then((res) => {
+    console.log(res.data)
+    // res.data.forEach((x) => {
+    //     console.log(x.categories)
+    // })
+    const computing = res.data.filter((item, index) => item.categories.toLowerCase().includes('computing') && index < 10)
+    const fashion = res.data.filter((item) => item.categories.toLowerCase().includes('fashion'))
+    const sporting = res.data.filter((item) => item.categories.toLowerCase().includes('sporting'))
+    const phones = res.data.filter((item) => item.categories.toLowerCase().includes('phones'))
+    const toys = res.data.filter((item) => item.categories.toLowerCase().includes('toys'))
+    console.log(computing)
+
+    computing.forEach((item) => {
+        const div = document.createElement('div')
+        div.className = 'item';
+        const img = document.createElement('img')
+        img.src = item['image']
+
+        const title = document.createElement('p')
+        title.innerHTML = item['displayName']
+
+        const price = document.createElement('p')
+        price.innerHTML = item['price']
+
+        div.appendChild(img)
+        div.appendChild(title)
+        div.appendChild(price)
+
+        computings.appendChild(div)
+    })
+}).catch((err) => {
+    console.error(err)
+}) //Asychronous Code
+
+//https://api.apify.com/v2/datasets/1LYsVC3JNAzgf6zBM/items?clean=true&format=json
 
